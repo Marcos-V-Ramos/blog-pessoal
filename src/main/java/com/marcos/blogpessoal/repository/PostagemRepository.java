726378a8e91ac18815c47d4a080395e3507c3487
Marcos Vinicius R <59856706+Marcos-V-Ramos@users.noreply.github.com>
2023-04-13 14:19:21 -0300

    Update PostagemRepository.java

--- src/main/java/com/marcos/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/marcos/blogpessoal/repository/PostagemRepository.java
@@ -2,7 +2,9 @@ package com.marcos.blogpessoal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.marcos.blogpessoal.entity.Postagem;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
 }
