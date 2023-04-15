7883dc41377160ecb85da5f942c076c634feeb28
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2023-04-14 23:57:17 -0300

    Subindo CRUD completo de postagens.

--- src/main/java/com/marcos/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/marcos/blogpessoal/repository/PostagemRepository.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.marcos.blogpessoal.entity.Postagem;
 
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
 	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
